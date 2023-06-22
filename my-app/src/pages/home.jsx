bea3b5df71834323dbaad6b501eda9ae7e15a873
ksuthant <ksuthant@uoguelph.ca>
2023-06-22 18:19:21 -0400

    Added in Service List and method for viewing application for full-screen windows

--- my-app/src/pages/home.jsx
+++ my-app/src/pages/home.jsx
@@ -1,11 +1,13 @@
 import './home.css'
 
-var IMAGES = [require('./images/redTruck.png'), require('./images/blueCar.png')];
+var IMAGES = [require('./images/redTruck.png'), require('./images/blueCar.png'), require('./images/serviceBlueCar.png'), require('./images/truck.png')
+, require('./images/redCheckMark.png')];
 
 
 function Home() {
     return (
         <html>
+            <div className="top-level-container">
             <body>
                 <header>
                     <div className="logoContainer">
@@ -24,16 +26,37 @@ function Home() {
                     <span className="subtext2"><p>Serving <b>Stouffville</b> and the <b>GTA</b></p></span>
                     <p className ="subtext3">25+ years of experience</p>
                 </div>
-                
+
                 <button className="contactButton">CONTACT US</button>
                 <button className="appointmentButton">BOOK APPOINTMENT</button>
                 <img className="img1" src={IMAGES[0]} alt=""></img>
                 <img className="img2" src={IMAGES[1]} alt=""></img>
 
                 <div className="services">
-                    <h2> Servicing </h2>
+                    <h2> <center>Servicing</center> </h2>
+                    <p className="servicingText"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                </div>
+
+                <div className="listOfServices">
+                    <h3> <center>Our Services "WORK IN PROGRESS"</center></h3>
+                    <p className="serviceLabel1">Some Label</p>
+                    <ul className="list1">
+                        <li className="serviceList"><span className="listText">Mechanical</span></li>
+                        <li className="serviceList"><span className="listText">Body Repair & Sheet Metal</span></li>
+                        <li className="serviceList"><span className="listText">Frame Repairs</span></li>
+                        <li className="serviceList"><span className="listText">Refinishing</span></li>
+                        <li className="serviceList"><span className="listText">Paint & Materials</span></li>
+                    </ul>
+
+                    <ul className="list2">
+                        <li className="serviceList"><span className="listText">Mechanical</span></li>
+                        <li className="serviceList"><span className="listText">Body Repair & Sheet Metal</span></li>
+                        <li className="serviceList"><span className="listText">Frame Repairs</span></li>
+                        <li className="serviceList"><span className="listText">Refinishing</span></li>
+                    </ul>
                 </div>
             </body>
+            </div>
         </html>
     )
 }
